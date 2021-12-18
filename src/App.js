75a3d1e9f6c88fd6d67cc69cbaddfc652e6c15b7
Ben Townsend <RBT000011@gmail.com>
2021-12-18 02:28:00 -0600

    removed un needed items

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import ContactMe from "./Components/ContactMe";
+import MyProjects from "./Components/MyProjects";
+import Navbar from "./Components/Navbar";
+import AboutMe from "./Components/AboutMe";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Navbar />
+
         <Switch>
           <Route exact path="/">
-            {About}
+            {AboutMe}
           </Route>
         </Switch>
         <Footer />
